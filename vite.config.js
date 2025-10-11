a6e97a6710f374d423e4142913e72be3efe766f9
CadetheCoder <cadehultz06@gmail.com>
2025-10-11 15:34:35 +0900

    Fixed config for new Router

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nike-react-store",
+  server: {
+    historyApiFallback: true, // fixes 404s on refresh/direct URLs
+  },
 })
